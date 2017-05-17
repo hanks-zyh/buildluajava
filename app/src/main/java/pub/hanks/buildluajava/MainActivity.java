1867b4138af6d72c1f87d76e17caee5a6e4cab8d
hanks-zyh <zhangyuhan2014@gmail.com>
2017-05-17 19:00:59 +0800

    build luajava to so files in Android Studio 2.3.2 modify file liolib.c add LuaJava java files

---
+++ app/src/main/java/pub/hanks/buildluajava/MainActivity.java
@@ -0,0 +1,25 @@
+package pub.hanks.buildluajava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // Example of a call to a native method
+    TextView tv = (TextView) findViewById(R.id.sample_text);
+    tv.setText("xxxxxxxxxx");
+    }
+
+
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("luajava");
+    }
+}
